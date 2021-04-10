4eab5a79155651a10dcc7e57d2674fa58ec6809d
Nazia R <nazia.huma@gmail.com>
2021-04-11 00:56:34 +0100

    set localstorage on submit

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+const getFromLocalStorage = () => {
+  const localStorageData = JSON.parse(localStorage.getItem("cities"));
+
+  if (localStorageData === null) {
+    return [];
+  } else {
+    return localStorageData;
+  }
+};
+
 const onSubmit = (event) => {
   event.preventDefault();
 
   const cityName = $("#city-input").val();
-  console.log(cityName);
+  //get cities array from local storage
+  const cities = getFromLocalStorage();
+  //push city name in to the array of cities
+  cities.push(cityName);
+
+  localStorage.setItem("cities", JSON.stringify(cities));
+
+  console.log(cities);
 };
 
 $("#search-by-city-form").on("submit", onSubmit);
